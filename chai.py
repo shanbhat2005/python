fe37ddcba9e699493fc093f4396ef6e677aca8a2
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-13 15:15:10 +0000

    program completed

---
+++ chai.py
@@ -0,0 +1,15 @@
+# arr = [2,5,1,7,4]
+
+# small= arr[0]
+
+# for i in arr:
+    
+#     if(small>i):
+#         small=i
+
+# print(small)
+
+area = int(input("enter the length of the side of square: "))
+
+answer= area*area
+print(answer)
